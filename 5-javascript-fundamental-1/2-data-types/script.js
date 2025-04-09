1393b254674ee3d0a6776b8b8167a4f18b2c98d1
zahinz <dev.huxnet@gmail.com>
2025-04-09 22:23:09 +0800

    finish data type

---
+++ 5-javascript-fundamental-1/2-data-types/script.js
@@ -0,0 +1,72 @@
+// 1. string
+// assigning a string to a variable
+// string is a sequence of characters
+// string can be enclosed in single or double quotes
+// string can be enclosed in backticks
+let firstName = "Ayda"; // sigle quotes is going to be formatted by prettier
+let lastName = "Zahin";
+const birthPlace = `Malaysia`;
+console.log(firstName);
+console.log(typeof lastName);
+console.log(lastName);
+console.log(typeof lastName);
+console.log(birthPlace);
+console.log(typeof birthPlace);
+
+// 2. number
+// assigning a number to a variable
+// number is a numeric value
+// number can be an integer or a floating point
+let age = 25; // integer
+// number can be a decimal
+let height = 5.5; // floating point
+// number can be a negative value
+let pressure = -60; // negative number
+// number can be a positive value
+let temperature = 36.5; // positive number
+
+const birthYear = 2022;
+
+console.log(age);
+console.log(typeof age);
+console.log(height);
+console.log(typeof height);
+console.log(pressure);
+console.log(typeof pressure);
+console.log(temperature);
+console.log(typeof temperature);
+console.log(birthYear);
+console.log(typeof birthYear);
+
+// 3. boolean
+// assigning a boolean to a variable
+// boolean can be true or false
+// conventionally veriables that are boolean should start with is, has, or can
+let isMarried = false; // boolean
+let hasPet = true; // boolean
+let canSwim = false; // boolean
+
+console.log(isMarried);
+console.log(typeof isMarried);
+console.log(hasPet);
+console.log(typeof hasPet);
+console.log(canSwim);
+console.log(typeof canSwim);
+
+// 4. null
+// assigning a null to a variable
+// null is a special value that represents no value
+// null is a primitive value
+
+let animal = null; // null
+console.log(animal);
+console.log(typeof animal);
+// null is an object
+
+// 5. undefined
+// assigning an undefined to a variable
+// undefined is a special value that represents no value
+// undefined is a primitive value
+let car; // undefined
+console.log(car);
+console.log(typeof car);
