20abd9680931df54e18662995f52190b4d74e7e7
zahinz <dev.huxnet@gmail.com>
2025-04-09 22:02:36 +0800

    finish variables

---
+++ 5-javascript-fundamental-1/1-variable-assignment/script.js
@@ -0,0 +1,38 @@
+console.log("Variable assignment");
+
+// Variable assignment
+// Using var
+// 1 declaring a variable
+// var firstName;
+var firstName;
+// 2 assigning a value
+// var firstName = "Ayda";
+firstName = "Ayda";
+// 3 using the variable
+// console.log(firstName);
+console.log(firstName);
+
+var lastName;
+lastName = "Zahin";
+console.log(lastName);
+
+// 4. reassigning a value
+// firstName = "Ali";
+firstName = "Ali";
+console.log(firstName);
+
+// 5. using let to declare a variable
+let age;
+age = 3;
+console.log(age);
+age = 4;
+console.log(age);
+
+// 6. using const to declare a variable we must assign a value straight away
+const birthPlace = "Malaysia";
+const birthYear = 2022;
+console.log(birthPlace);
+console.log(birthYear);
+// try to reassign a value to a const variable
+// birthPlace = "Singapore";
+// console.log(birthPlace);
