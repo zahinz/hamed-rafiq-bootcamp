25cd17edb66f2b9fd5b67fa6732873cfc3bd9988
zahinz <dev.huxnet@gmail.com>
2025-09-03 22:57:41 +0800

    Implement visitor count tracking and update home page to display visitor number

--- 13-express-js/controllers/index.js
+++ 13-express-js/controllers/index.js
@@ -1,14 +1,35 @@
 import path from "path";
 import { __dirname } from "../utils/index.js";
+import fs from "fs";
+
+// store the number of visitors using a variable
+// variable is stored in the memory (RAM) of the server
+// it will reset when the server restarts
+// let visitorCount = 0;
+let visitorCountPath = path.join(__dirname, "visitor_count.txt");
+console.log(visitorCountPath);
+// read the visitor count from the file and parse it to integer
+let visitorCount = parseInt(fs.readFileSync(visitorCountPath, "utf-8"));
 
 export function homePage(req, res) {
+  // increment the visitor count
+  visitorCount++;
+  // write the visitor count to the file
+  fs.writeFileSync(visitorCountPath, visitorCount.toString());
+  console.log("Home page visited", visitorCount);
+
   // how to server a static html file
   // get the absolute path of the html file
   // path is core module of nodejs which we imported above
   // path module helps to create path in a cross platform way (mac - /pages/index.html, windows - \pages\index.html)
   const htmlPath = path.join(__dirname, "pages", "index.html");
   // respond with the html file using sendFile method of res object
-  res.sendFile(htmlPath);
+  // get the content of the html file
+  let htmlContent = fs.readFileSync(htmlPath, "utf-8");
+  // replace the placeholder with the visitor count
+  htmlContent = htmlContent.replace("[[COUNT]]", visitorCount);
+  // send the modified html content
+  res.send(htmlContent);
 }
 
 export function contactPage(req, res) {
